8b6096b5710392ab7efe50e23df9583036e903e8
Richard Wurth <richard.wurth91@gmail.com>
2017-01-28 11:43:06 -0700

    Initial and final submission for sayName

--- sayName.js
+++ sayName.js
@@ -2,12 +2,25 @@
 
   //code here
 
+const Person = function (name,age) {
+  this.name = name;
+  this.age = age;
+}
 
 //Now create three instances of Person with data you make up
 
   //code here
 
+let william = new Person ("William", 25);
+let robert = new Person ("Robert", 53);
+let james = new Person ("James", 12);
 
 //Now add a sayName method on your Person class that will alert the name of whatever Person instance called it.
 
   //code here
+
+// console.log(william);
+
+Person.prototype.sayName = function() {
+  alert(this.name);
+}
